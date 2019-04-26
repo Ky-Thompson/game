75870af8411161f842484980806dd468a2d3e131
Samuel Fernandez <SFernandez@jw.org>
2019-04-26 13:49:41 +0200

    Move all styles to scss files

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+const uncss = require('uncss');
 
 module.exports = {
-  plugins: [autoprefixer, cssnano({ preset: 'default' })],
+  plugins: [autoprefixer, cssnano({ preset: 'default' }), uncss.postcssPlugin({ html: ['./src/index.html'] })],
 };
--- webpack.config.js
+++ webpack.config.js
@@ -14,49 +14,56 @@ const PHASER_PRODUCTION = 'node_modules/phaser/dist/phaser.min.js';
 module.exports.DEVELOPMENT_MODE = DEVELOPMENT_MODE;
 module.exports.PRODUCTION_MODE = PRODUCTION_MODE;
 
-module.exports.webpackConfig = (mode) => ({
-  entry: './src/main.ts',
-  output: {
-    path: join(__dirname, 'dist'),
-    filename: mode === PRODUCTION_MODE ? '[name].[chunkhash].js' : '[name].js',
-    publicPath: '',
-  },
-  mode: mode || PRODUCTION_MODE,
-  module: {
-    rules: [
-      { test: /\.ts$/, loader: 'ts-loader', include: join(__dirname, 'src'), exclude: '/node_modules/' },
-      { test: /phaser\.js$/, loader: 'expose-loader?Phaser' },
-      { test: [/\.vert$/, /\.frag$/], use: 'raw-loader' },
-      { test: /\.css$/, use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader'] },
-      { test: /\.scss$/, use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'] },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-    alias: {
-      phaser: join(__dirname, mode === PRODUCTION_MODE ? PHASER_PRODUCTION : PHASER_DEVELOPMENT),
+module.exports.webpackConfig = (mode) => {
+  const isProduction = mode === PRODUCTION_MODE;
+  const scssLoaders = isProduction
+    ? ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+    : ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'];
+  const phaserScript = isProduction ? PHASER_PRODUCTION : PHASER_DEVELOPMENT;
+
+  return {
+    entry: './src/main.ts',
+    output: {
+      path: join(__dirname, 'dist'),
+      filename: mode === PRODUCTION_MODE ? '[name].[chunkhash].js' : '[name].js',
+      publicPath: '',
     },
-    plugins: [new TsconfigPathsPlugin()],
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
-      WEBGL_RENDERER: JSON.stringify(true),
-      CANVAS_RENDERER: JSON.stringify(true),
-    }),
-    new MiniCssExtractPlugin({
-      filename: mode === PRODUCTION_MODE ? 'style.[hash].css' : 'style.css',
-      chunkFilename: mode === PRODUCTION_MODE ? '[id].[hash].css' : '[id].css',
-    }),
-    new HtmlWebpackPlugin({
-      hash: true,
-      template: join(__dirname, 'src/index.html'),
-      filename: join(__dirname, 'dist/index.html'),
-    }),
-    new BundleAnalyzerPlugin({ analyzerMode: 'static', openAnalyzer: false }),
-  ],
-  performance: {
-    maxEntrypointSize: 1500000, // 1.5MB
-    maxAssetSize: 1500000, // 1.5MB
-  },
-});
+    mode: mode || PRODUCTION_MODE,
+    module: {
+      rules: [
+        { test: /\.ts$/, loader: 'ts-loader', include: join(__dirname, 'src'), exclude: '/node_modules/' },
+        { test: /phaser\.js$/, loader: 'expose-loader?Phaser' },
+        { test: [/\.vert$/, /\.frag$/], use: 'raw-loader' },
+        { test: /\.scss$/, use: scssLoaders },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+      alias: {
+        phaser: join(__dirname, phaserScript),
+      },
+      plugins: [new TsconfigPathsPlugin()],
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
+        WEBGL_RENDERER: JSON.stringify(true),
+        CANVAS_RENDERER: JSON.stringify(true),
+      }),
+      new MiniCssExtractPlugin({
+        filename: isProduction ? 'style.[hash].css' : 'style.css',
+        chunkFilename: isProduction ? '[id].[hash].css' : '[id].css',
+      }),
+      new HtmlWebpackPlugin({
+        hash: true,
+        template: join(__dirname, 'src/index.html'),
+        filename: join(__dirname, 'dist/index.html'),
+      }),
+      new BundleAnalyzerPlugin({ analyzerMode: 'static', openAnalyzer: false }),
+    ],
+    performance: {
+      maxEntrypointSize: 1500000, // 1.5MB
+      maxAssetSize: 1500000, // 1.5MB
+    },
+  };
+};
