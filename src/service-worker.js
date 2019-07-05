70a504497a6ca81bdd1a1b7a24fcd5e23d80c3e9
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2019-07-05 11:08:43 +0200

    Fix workbox (#130)
    * Update tsconfig
    
    * Fix version workbox

--- src/service-worker.js
+++ src/service-worker.js
@@ -5,11 +5,12 @@ if (workbox) {
   workbox.precaching.precacheAndRoute([]);
 
   // Store in the cache all assets
-  workbox.routing.registerRoute(
-    new RegExp('\\.(js|html|css|ico|png|xml|json|ac3|m4a|mp3|ogg|fnt)$'),
-    new workbox.strategies.NetworkFirst()
-  );
+  workbox.routing.registerRoute(new RegExp('\\.(css|ico|png|xml|json|ac3|m4a|mp3|ogg|fnt)$'), new workbox.strategies.NetworkFirst());
+  workbox.routing.registerRoute(new RegExp('https://storage.googleapis.com/'), new workbox.strategies.NetworkFirst());
 
   // Enable Offline Google Analytics
   workbox.googleAnalytics.initialize();
+
+  // Skip waiting
+  workbox.core.skipWaiting();
 }
